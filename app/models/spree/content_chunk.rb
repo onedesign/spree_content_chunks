867c42ec38fdd31ce31cd0fed5149e2eec77e444
Brice Sanchez <brice.sanchez@nurun.com>
2013-03-06 10:08:18 -0500

    refactor attr_accessible with :image and :delete_image

--- app/models/spree/content_chunk.rb
+++ app/models/spree/content_chunk.rb
@@ -3,11 +3,10 @@ class Spree::ContentChunk < ActiveRecord::Base
                     :url => "/spree/content_chunks/:id/main/:style/:basename.:extension",
                     :path => ":rails_root/public/spree/content_chunks/:id/main/:style/:basename.:extension"
 
+  attr_accessible :title, :body, :link, :kind, :image, :delete_image
   attr_accessor :delete_image
   before_validation { image.clear if '1' == delete_image }
 
   validates :title, :presence => true
   validates :kind, :presence => true
-
-  attr_accessible :title, :body, :link, :kind, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :image_position
 end
