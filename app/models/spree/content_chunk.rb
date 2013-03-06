f1c7a8bb78dc10e7d85ad4c0d8f4473c4f70ecd6
Brice Sanchez <brice.sanchez@nurun.com>
2013-03-06 10:59:04 -0500

    Add :image_position to attr_accessible in content_chunk model

--- app/models/spree/content_chunk.rb
+++ app/models/spree/content_chunk.rb
@@ -3,7 +3,7 @@ class Spree::ContentChunk < ActiveRecord::Base
                     :url => "/spree/content_chunks/:id/main/:style/:basename.:extension",
                     :path => ":rails_root/public/spree/content_chunks/:id/main/:style/:basename.:extension"
 
-  attr_accessible :title, :body, :link, :kind, :image, :delete_image
+  attr_accessible :title, :body, :link, :kind, :image, :image_position, :delete_image
   attr_accessor :delete_image
   before_validation { image.clear if '1' == delete_image }
 
